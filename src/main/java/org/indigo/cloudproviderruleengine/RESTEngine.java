b4d1fd7ba9b68c1769b7023daeb76a3c008b6c71
Alvise Dorigo <alvise72@gmail.com>
2016-03-10 13:28:14 +0100

    parametrized TCP listening port; now can be passed as first argument after RESTEngine class

--- src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
+++ src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
@@ -25,32 +25,25 @@ import com.google.gson.*;
  */
 public class RESTEngine {
 
-	/**
-	 * TO TEST: java -cp CloudProviderRuleEngine.jar:$HOME/src/gson-2.6.1.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/slf4j-api.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-internal:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-api.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-api-6.3.0.Final.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-internal-6.3.0.Final.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/drools-compiler.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/drools-core.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/slf4j-api.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/xstream.jar:/home/dorigoa/src/OEPE/plugins/org.eclipse.jdt.core_3.11.1.v20150902-1521.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/mvel2.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/antlr-runtime.jar org.indigo.cloudproviderruleengine.RESTEngine
-	 * 
-	 * @param args
-	 */
-	
-	/*
-	 * 
-	 * To Run: java -cp CloudProviderRuleEngine.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-internal:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-api.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-api-6.3.0.Final.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/kie-internal-6.3.0.Final.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/drools-compiler.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/drools-core.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/slf4j-api.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/xstream.jar:/home/dorigoa/src/OEPE/plugins/org.eclipse.jdt.core_3.11.1.v20150902-1521.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/mvel2.jar:$HOME/src/droolsjbpm-tools-distribution-6.3.0.Final/binaries/antlr-runtime.jar org.indigo.cloudproviderruleengine.RESTEngine
-	 * 
-	 */
     public static final void main(String[] args) {
     	
+	int TCPPORT = 8080;
+	if(args.length>0)
+	    TCPPORT = Integer.parseInt( args[0] );
+
     	HttpServer server = null;
     	try {
-			server = HttpServer.create(new InetSocketAddress(8080), 0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	    server = HttpServer.create(new InetSocketAddress(TCPPORT), 0);
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
     	if(server != null) {
-    		//server.createContext("/echo", new echoHandler());
-    		server.createContext("/version", new versionHandler());
-    		server.createContext("/rank", new rankHandler());
-    		server.setExecutor(null); // creates a default executor
-    		server.start();
+	    server.createContext("/version", new versionHandler());
+	    server.createContext("/rank", new rankHandler());
+	    server.setExecutor(null); // creates a default executor
+	    server.start();
+	    System.out.println("HTTP Server is listening on port "+TCPPORT+"\n");
     	}
     }
     
@@ -64,7 +57,7 @@ public class RESTEngine {
     static class versionHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-        	String response = "v0.1\n";
+	    String response = "v0.1\n";
             t.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -82,83 +75,83 @@ public class RESTEngine {
     static class rankHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-        	InputStream is = t.getRequestBody();
-        	InputStreamReader inputReader = new InputStreamReader(is,"utf-8");
-        	BufferedReader buffReader = new BufferedReader(inputReader);
-        	String line = buffReader.readLine();
-        	
-        	List<CloudProvider> cpvec = null;
-    		try {
-    		  Gson gson = new Gson();
-    		  JsonElement E = gson.fromJson(line, JsonElement.class);
-        	  JsonObject obj = E.getAsJsonObject( );
-        	  if ( obj.has("cloudproviders") ) {
-        		  cpvec = parse( obj.get("cloudproviders").getAsJsonArray( ) );
-          	  }
-    		} catch(Exception e) {
-    			String err = "Exception parsing JSON client request: " + e.getMessage() + "\n";
-    			t.sendResponseHeaders(400, err.getBytes().length);
-    			OutputStream os = t.getResponseBody();
-    			os.write(err.getBytes());
-    			os.close();
-    			e.printStackTrace();
-    		} catch(Throwable e) {
-    			String err = "Throwable parsing JSON client request: " + e.getMessage() + "\n";
-    			t.sendResponseHeaders(400, err.getBytes().length);
-    			OutputStream os = t.getResponseBody();
-    			os.write(err.getBytes());
-    			os.close();
-    			e.printStackTrace();
-    		}
-    		
-    		KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
-        	
-        	for(CloudProvider cp : cpvec ) {
-        		kSession.insert(cp);
-        	}
-        	
-    		int tot = kSession.fireAllRules();
-    		System.out.println("Total rules applied="+tot+"\n");
-    		
-    		Vector<String> respVec = new Vector<String>();
-    		for(CloudProvider cp : cpvec ) {
-    			RankedCloudProvider rcp = new RankedCloudProvider( cp.getID(), cp.getName(), cp.getTotalRank() );
-    			Gson gson = new Gson();
-    			String json = gson.toJson(rcp);
-    			respVec.add(json);
-    			
-        	}
-    		String response = "{\"rankedcloudproviders\":[" + String.join(",", respVec) + "]}";
-    		System.out.println(response+"\n\n");
-    		t.sendResponseHeaders(200, response.getBytes().length);
+	    InputStream is = t.getRequestBody();
+	    InputStreamReader inputReader = new InputStreamReader(is,"utf-8");
+	    BufferedReader buffReader = new BufferedReader(inputReader);
+	    String line = buffReader.readLine();
+	    
+	    List<CloudProvider> cpvec = null;
+	    try {
+		Gson gson = new Gson();
+		JsonElement E = gson.fromJson(line, JsonElement.class);
+		JsonObject obj = E.getAsJsonObject( );
+		if ( obj.has("cloudproviders") ) {
+		    cpvec = parse( obj.get("cloudproviders").getAsJsonArray( ) );
+		}
+	    } catch(Exception e) {
+		String err = "Exception parsing JSON client request: " + e.getMessage() + "\n";
+		t.sendResponseHeaders(400, err.getBytes().length);
+		OutputStream os = t.getResponseBody();
+		os.write(err.getBytes());
+		os.close();
+		e.printStackTrace();
+	    } catch(Throwable e) {
+		String err = "Throwable parsing JSON client request: " + e.getMessage() + "\n";
+		t.sendResponseHeaders(400, err.getBytes().length);
+		OutputStream os = t.getResponseBody();
+		os.write(err.getBytes());
+		os.close();
+		e.printStackTrace();
+	    }
+	    
+	    KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.getKieClasspathContainer();
+	    KieSession kSession = kContainer.newKieSession("ksession-rules");
+	    for(CloudProvider cp : cpvec )
+		kSession.insert(cp);
+	    
+	    int tot = kSession.fireAllRules();
+	    System.out.println("Total rules applied="+tot+"\n");
+	    
+	    Vector<String> respVec = new Vector<String>();
+	    for(CloudProvider cp : cpvec ) {
+		RankedCloudProvider rcp = new RankedCloudProvider( cp.getID(), cp.getName(), cp.getTotalRank() );
+		Gson gson = new Gson();
+		String json = gson.toJson(rcp);
+		respVec.add(json);
+	    }
+	    String response = "{\"rankedcloudproviders\":[" + String.join(",", respVec) + "]}";
+	    System.out.println(response+"\n\n");
+	    t.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-    		
+	    
         }
     }
     
+    /**
+     *
+     *
+     * Convert JsonObject to CloudProvider object
+     *
+     *
+     */
     static List<CloudProvider> parse(JsonArray array) {
     	List<CloudProvider> cpvec = new Vector<CloudProvider>( );
-    	
         for(int i = 0; i < array.size( ); i++) {
-    	  try {
-    		  JsonObject obj = array.get( i ).getAsJsonObject();
-    		  System.out.println("Processing provider ["+obj.toString());
-    		  
-    			  //System.out.println("Provider OK !");
-    			  Gson gson = new GsonBuilder().create();
-    	          CloudProvider cp = gson.fromJson(obj, CloudProvider.class);
-    			  cpvec.add(cp);
+	    try {
+		JsonObject obj = array.get( i ).getAsJsonObject();
+		System.out.println("Processing provider ["+obj.toString());
+		Gson gson = new GsonBuilder().create();
+		CloudProvider cp = gson.fromJson(obj, CloudProvider.class);
+		cpvec.add(cp);
     	  } catch(Exception e) {
-    		  System.out.println(e.getMessage());
-  		} catch(Throwable t) {
-  			System.out.println(t.getMessage());
-  		} 
-      }
-      
-      return cpvec;
+		System.out.println(e.getMessage());
+	    } catch(Throwable t) {
+		System.out.println(t.getMessage());
+	    } 
+	}
+	return cpvec;
     }
 }

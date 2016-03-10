6e5a81169d3f2a2e49829512aad60112c7445ed5
Alvise Dorigo <alvise72@gmail.com>
2016-03-10 13:56:09 +0100

    wrapping all Kie's call into try/catch

--- src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
+++ src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
@@ -75,19 +75,43 @@ public class RESTEngine {
     static class rankHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-	    InputStream is = t.getRequestBody();
-	    InputStreamReader inputReader = new InputStreamReader(is,"utf-8");
-	    BufferedReader buffReader = new BufferedReader(inputReader);
-	    String line = buffReader.readLine();
+	    
 	    
 	    List<CloudProvider> cpvec = null;
 	    try {
+		InputStream is = t.getRequestBody();
+		InputStreamReader inputReader = new InputStreamReader(is,"utf-8");
+		BufferedReader buffReader = new BufferedReader(inputReader);
+		String line = buffReader.readLine();
 		Gson gson = new Gson();
 		JsonElement E = gson.fromJson(line, JsonElement.class);
 		JsonObject obj = E.getAsJsonObject( );
 		if ( obj.has("cloudproviders") ) {
 		    cpvec = parse( obj.get("cloudproviders").getAsJsonArray( ) );
 		}
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieSession kSession = kContainer.newKieSession("ksession-rules");
+		for(CloudProvider cp : cpvec )
+		    kSession.insert(cp);
+		
+		int tot = kSession.fireAllRules();
+		System.out.println("Total rules applied="+tot+"\n");
+		
+		Vector<String> respVec = new Vector<String>();
+		for(CloudProvider cp : cpvec ) {
+		    RankedCloudProvider rcp = new RankedCloudProvider( cp.getID(), cp.getName(), cp.getTotalRank() );
+		    Gson gson2 = new Gson();
+		    String json = gson2.toJson(rcp);
+		    respVec.add(json);
+		}
+		String response = "{\"rankedcloudproviders\":[" + String.join(",", respVec) + "]}";
+		System.out.println(response+"\n\n");
+		t.sendResponseHeaders(200, response.getBytes().length);
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+
 	    } catch(Exception e) {
 		String err = "Exception parsing JSON client request: " + e.getMessage() + "\n";
 		t.sendResponseHeaders(400, err.getBytes().length);
@@ -95,6 +119,7 @@ public class RESTEngine {
 		os.write(err.getBytes());
 		os.close();
 		e.printStackTrace();
+
 	    } catch(Throwable e) {
 		String err = "Throwable parsing JSON client request: " + e.getMessage() + "\n";
 		t.sendResponseHeaders(400, err.getBytes().length);
@@ -104,28 +129,6 @@ public class RESTEngine {
 		e.printStackTrace();
 	    }
 	    
-	    KieServices ks = KieServices.Factory.get();
-	    KieContainer kContainer = ks.getKieClasspathContainer();
-	    KieSession kSession = kContainer.newKieSession("ksession-rules");
-	    for(CloudProvider cp : cpvec )
-		kSession.insert(cp);
-	    
-	    int tot = kSession.fireAllRules();
-	    System.out.println("Total rules applied="+tot+"\n");
-	    
-	    Vector<String> respVec = new Vector<String>();
-	    for(CloudProvider cp : cpvec ) {
-		RankedCloudProvider rcp = new RankedCloudProvider( cp.getID(), cp.getName(), cp.getTotalRank() );
-		Gson gson = new Gson();
-		String json = gson.toJson(rcp);
-		respVec.add(json);
-	    }
-	    String response = "{\"rankedcloudproviders\":[" + String.join(",", respVec) + "]}";
-	    System.out.println(response+"\n\n");
-	    t.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
 	    
         }
     }

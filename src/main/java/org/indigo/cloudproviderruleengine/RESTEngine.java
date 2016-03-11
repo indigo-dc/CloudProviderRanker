0ae1fd88dc1cdf3afec5aca96a99f8a6c565172c
Alvise Dorigo <alvise72@gmail.com>
2016-03-11 14:25:09 +0100

    added thread pool to execute the WEB Server

--- src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
+++ src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
@@ -2,8 +2,15 @@ package org.indigo.cloudproviderruleengine;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsServer;
+import javax.net.ssl.SSLContext;
+import com.sun.net.httpserver.HttpsConfigurator;
+import java.security.KeyStore;
+import java.io.FileInputStream;
+
 
 /**
  * This is a sample class to launch a rule.
@@ -13,24 +20,29 @@ public class RESTEngine {
     public static final void main(String[] args) {
     	
 	int TCPPORT = 8080;
+	boolean usessl = false;
+
 	if(args.length>0)
 	    TCPPORT = Integer.parseInt( args[0] );
-
-    	HttpServer server = null;
-    	try {
-	    server = HttpServer.create(new InetSocketAddress(TCPPORT), 0);
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	if(args.length>1)
+	    usessl = true;
+	if(!usessl) {
+	    HttpServer server = null;
+	    try {
+		server = HttpServer.create(new InetSocketAddress(TCPPORT), 0);
+	    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    if(server != null) {
+		server.createContext("/version", new VersionHandler());
+		server.createContext("/rank", new RankHandler());
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.start();
+		System.out.println("HTTP Server is listening on port "+TCPPORT+"\n");
+	    }
+	} else {
+	    
 	}
-    	if(server != null) {
-	    server.createContext("/version", new VersionHandler());
-	    server.createContext("/rank", new RankHandler());
-	    server.setExecutor(null); // creates a default executor
-	    server.start();
-	    System.out.println("HTTP Server is listening on port "+TCPPORT+"\n");
-    	}
     }
-    
-    
 }

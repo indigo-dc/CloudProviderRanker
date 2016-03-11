8405a255a4255f985adf9e35a4ed6483a1436925
Alvise Dorigo <alvise72@gmail.com>
2016-03-11 15:39:37 +0100

    passing keystore file and password by command line

--- src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
+++ src/main/java/org/indigo/cloudproviderruleengine/RESTEngine.java
@@ -1,22 +1,24 @@
 package org.indigo.cloudproviderruleengine;
 
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import java.util.concurrent.Executors;
 import java.util.Map;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpsServer;
+
 import java.net.InetSocketAddress;
 import javax.net.ssl.SSLContext;
 import com.sun.net.httpserver.HttpsConfigurator;
 import java.security.KeyStore;
-import java.io.FileInputStream;
 import javax.net.ssl.KeyManagerFactory;
 import java.security.NoSuchAlgorithmException;
-import java.io.FileNotFoundException;
 import java.security.KeyStoreException;
-import java.io.IOException;
+
 import java.security.cert.CertificateException;
 import java.security.UnrecoverableKeyException;
 import java.security.KeyManagementException;
@@ -34,26 +36,27 @@ public class RESTEngine {
 	int TCPPORT = 8080;
 	boolean usessl = false;
 	HttpServer server = null;
+	String keystorepath = null;
+	String password = null;
 
 	if(args.length>0)
 	    TCPPORT = Integer.parseInt( args[0] );
-	if(args.length>1)
+	if(args.length>=2 && args.length <3) {
+	    System.out.println("If a keystore path is specified then must specify also a password. STOP!");
+	    System.exit(1);
+	}
+	if(args.length == 3) {
 	    usessl = true;
+	    keystorepath = args[1];
+	    password = args[2];
+	}
 	if(!usessl) {
 	    
 	    try {
 		server = HttpServer.create(new InetSocketAddress(TCPPORT), 0);
 	    } catch (IOException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	    }
-// 	    if(server != null) {
-// 		server.createContext("/version", new VersionHandler());
-// 		server.createContext("/rank", new RankHandler());
-// 		server.setExecutor(Executors.newCachedThreadPool());
-// 		server.start();
-// 		System.out.println("HTTP Server is listening on port "+TCPPORT+"\n");
-// 	    }
 	} else {
 
 	    // Use: keytool -genkey -alias webservice -keystore server_keystore.ks
@@ -62,7 +65,6 @@ public class RESTEngine {
 	    try {
 		server = HttpsServer.create(new InetSocketAddress(TCPPORT), 0);
 	    } catch (IOException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	    }
 	    SSLContext sslContext = null;
@@ -72,7 +74,7 @@ public class RESTEngine {
 		e.printStackTrace();
 	    }
 	    
-	    char[] keystorePassword = "mypassword".toCharArray( );
+	    char[] keystorePassword = password.toCharArray( );
 	    KeyStore ks = null;
 	    try {
 		ks = KeyStore.getInstance("JKS");
@@ -80,7 +82,7 @@ public class RESTEngine {
 		e.printStackTrace();
 	    }
 	    try {
-		ks.load(new FileInputStream(home + "/server_keystore.ks"), keystorePassword);
+		ks.load(new FileInputStream(keystorepath), keystorePassword);
 	    } catch(FileNotFoundException e) {
 		e.printStackTrace();
 	    } catch(IOException e) {
@@ -115,11 +117,6 @@ public class RESTEngine {
 
 	    HttpsConfigurator configurator = new HttpsConfigurator(sslContext);
 	    ((HttpsServer)server).setHttpsConfigurator(configurator);
-	    //server.createContext("/version", new VersionHandler());
-	    //server.createContext("/rank", new RankHandler());
-	    //server.setExecutor(Executors.newCachedThreadPool());
-	    //server.start();
-	    //System.out.println("HTTPS Server is listening on port "+TCPPORT+"\n");
 	}
 
 	server.createContext("/version", new VersionHandler());

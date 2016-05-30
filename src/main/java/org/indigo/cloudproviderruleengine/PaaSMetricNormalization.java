4336214a3138b9fd68c2e87bda9ca873aa5ded9d
Alvise Dorigo <alvise72@gmail.com>
2016-05-30 12:32:40 +0200

    new stuff for normalization data for paas metric

---
+++ src/main/java/org/indigo/cloudproviderruleengine/PaaSMetricNormalization.java
@@ -0,0 +1,6 @@
+package org.indigo.cloudproviderruleengine;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class PaaSMetricNormalization {
+  
+}
--- src/main/java/org/indigo/cloudproviderruleengine/PaaSMetricRanked.java
+++ src/main/java/org/indigo/cloudproviderruleengine/PaaSMetricRanked.java
@@ -19,6 +19,30 @@ public class PaaSMetricRanked extends PaaSMetric {
 	  for(int i = 0; i < array.size( ); ++i ) {
 	    paaSMetricRanked.add( (PaaSMetricRanked)(new GsonBuilder().create()).fromJson(array.get( i ).getAsJsonObject(), PaaSMetricRanked.class) );
 	  }
+	  if(normalization_file != null) {
+	    InputStream is = null;
+    	    String Line = "";
+    	    try {
+      	      is = new FileInputStream(new File(normalization_file));
+    
+    
+	      InputStreamReader inputReader = new InputStreamReader(is);
+     	      BufferedReader buffReader     = new BufferedReader(inputReader);
+    	      //String Line = "";
+    	      String line = "";
+    	      while( (line = buffReader.readLine()) != null) {
+    	        Line += line;
+    	      }
+   	    } catch(Exception e) {
+	      return new PaaSMetricNormalizations(1,1,1,1,1);
+   	    } catch(Throwable t) {
+              return new PaaSMetricNormalizations(1,1,1,1,1);  
+   	    }
+	  }
+	  Gson gson = new Gson();
+   	  //System.err.println("SlaPriorities - Line="+Line);
+    	  JsonElement E = gson.fromJson(Line, JsonElement.class);
+    	  //return (SlaNormalizations)gson.fromJson(E.getAsJsonObject( ), SlaNormalizations.class);
 	  return paaSMetricRanked;
 	}
 	
@@ -26,4 +50,6 @@ public class PaaSMetricRanked extends PaaSMetric {
  	public String toString( ) {
  	  return ToStringBuilder.reflectionToString(this);
  	}
+	
+	
 }

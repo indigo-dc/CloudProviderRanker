6c49807c57768f7384e7e76284ee6a265a9fe4d6
Alvise Dorigo <alvise72@gmail.com>
2016-05-31 11:58:10 +0200

    added unit test

---
+++ src/test/java/org/indigo/cloudproviderruleengine/test/TargetTest.java
@@ -0,0 +1,16 @@
+package org.indigo.cloudproviderruleengine.test;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.indigo.cloudproviderruleengine.Target;
+import org.indigo.cloudproviderruleengine.Restrictions;
+
+public class TargetTest {
+  @Test
+  public void test( ) {
+    Target t = new Target( );
+    assertTrue( null!=t ); 
+  }
+
+}

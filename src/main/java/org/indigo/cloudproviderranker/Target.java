e5f810b5c9066496ff3566205a192eae97c73ce8
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2017-01-14 17:38:38 +0100

    fix for codestyle

--- src/main/java/org/indigo/cloudproviderranker/Target.java
+++ src/main/java/org/indigo/cloudproviderranker/Target.java
@@ -1,4 +1,4 @@
- package org.indigo.cloudproviderranker;
+package org.indigo.cloudproviderranker;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Target {
@@ -8,7 +8,6 @@ public class Target {
   
   @Override
   public String toString( ) {
-    //return "type=" + type + ", unit=" + unit + ", restritions={" + restrictions.toString( )+"}";
     return ToStringBuilder.reflectionToString(this);
   }
 }

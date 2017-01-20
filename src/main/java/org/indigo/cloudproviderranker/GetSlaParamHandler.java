bca07703808834259d102296c0c8967287df7652
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2017-01-20 11:11:04 +0100

    codestyle fix

--- src/main/java/org/indigo/cloudproviderranker/GetSlaParamHandler.java
+++ src/main/java/org/indigo/cloudproviderranker/GetSlaParamHandler.java
@@ -1,7 +1,7 @@
 package org.indigo.cloudproviderranker;
 
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;

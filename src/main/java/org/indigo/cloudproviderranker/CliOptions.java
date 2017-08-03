ba13f7866fead48c01ba6016568b6e1acd459454
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2017-08-03 12:04:14 +0200

    Define CLI options
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

---
+++ src/main/java/org/indigo/cloudproviderranker/CliOptions.java
@@ -0,0 +1,41 @@
+package org.indigo.cloudproviderranker;
+
+import com.google.devtools.common.options.Option;
+import com.google.devtools.common.options.OptionsBase;
+
+/**
+ * Command-line options definition for CPR.
+ */
+public final class CliOptions extends OptionsBase {
+
+  @Option(
+      name = "help",
+      abbrev = 'h',
+      help = "Prints usage info.",
+      defaultValue = "true"
+  ) public boolean help;
+
+  @Option(
+      name = "port",
+      abbrev = 'p',
+      help = "The server port.",
+      category = "startup",
+      defaultValue = "8080"
+  ) public int port;
+
+  @Option(
+      name = "keystore-path",
+      abbrev = 'k',
+      help = "Keystore path.",
+      category = "startup",
+      defaultValue = ""
+  ) public String keystorePath;
+
+  @Option(
+      name = "password",
+      abbrev = 'P',
+      help = "Keystore password.",
+      category = "startup",
+      defaultValue = ""
+  ) public String password;
+}

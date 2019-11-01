79283136a3be07eeff7412f9e01ecab111e0a3d3
Marica Antonacci <marica.antonacci@ba.infn.it>
2019-11-01 23:10:57 +0100

    Added toJsonString() method

--- src/main/java/org/indigo/cloudproviderranker/utils/Utils.java
+++ src/main/java/org/indigo/cloudproviderranker/utils/Utils.java
@@ -1,14 +1,28 @@
 package org.indigo.cloudproviderranker.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.indigo.cloudproviderranker.dto.Metric;
 
+
 public class Utils {
 
   public static Map<String, Float> metricsToMap(List<Metric> m) {
     return m.stream().collect(Collectors.toMap(Metric::getMetricName, Metric::getMetricValue));
+  }
+  
+  public static <T> String toJsonString(T obj) {
+	  try {
+			ObjectMapper objmapper = new ObjectMapper();
+			return objmapper.writeValueAsString(obj);
+	  }catch (JsonProcessingException e)
+	  {
+		  return "";
+	  }
   }
 }
